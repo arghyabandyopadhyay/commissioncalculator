0af9cfa9620a9df7a9adefccf7435a15210f6e7a
arghyabandyopadhyay <arghyab13bga@gmail.com>
2021-04-12 19:46:53 +0530

    apr12 update

--- lib/main.dart
+++ lib/main.dart
@@ -29,10 +29,31 @@ class _MyHomePageState extends State<MyHomePage> {
   double wholeSalersShare = 0;
   double myShare = 0;
   double chemistsShare = 0;
+  double chemistsPrice = 0;
   double doctorsShare = 0;
+  double doctorsPrice = 0;
+  double mrp=0;
+  double cp=0;
+  double wscomm=0;
+  double scheme=0;
+  double margin=0;
   TextEditingController costPriceController=TextEditingController();
   TextEditingController mrpPriceController=TextEditingController();
   TextEditingController wholeSalersCommissionController=TextEditingController();
+  TextEditingController chemistSchemeController=TextEditingController();
+  void calculateValues(){
+    mrp=(mrpPriceController.text!=""?double.parse(mrpPriceController.text):0);
+    cp=(costPriceController.text!=""?double.parse(costPriceController.text):0);
+    wscomm=(wholeSalersCommissionController.text!=""?double.parse(wholeSalersCommissionController.text)/100:0.2);
+    scheme=((chemistSchemeController.text!=""?double.parse(chemistSchemeController.text):0)/100);
+    margin=mrp-cp;
+    wholeSalersShare=margin*wscomm;
+    chemistsShare=margin*0.20;
+    doctorsShare=margin*0.30;
+    chemistsPrice=(mrp-chemistsShare)*(1-scheme);
+    myShare=margin-wholeSalersShare-(chemistsShare+scheme*(mrp-chemistsShare))-doctorsShare;
+    doctorsPrice=chemistsPrice-doctorsShare;
+  }
   @override
   void initState() {
     super.initState();
@@ -56,11 +77,7 @@ class _MyHomePageState extends State<MyHomePage> {
             style: TextStyle(fontSize: 30),
             onChanged: (value){
               setState(() {
-                double temp=(mrpPriceController.text!=""?double.parse(mrpPriceController.text):0)-(value!=""?double.parse(value):0);
-                wholeSalersShare=temp*0.20;
-                chemistsShare=temp*0.20;
-                doctorsShare=temp*0.30;
-                myShare=temp-wholeSalersShare-chemistsShare-doctorsShare;
+                calculateValues();
               });
             },
           ),
@@ -75,11 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
             onChanged: (value)
             {
               setState(() {
-                double temp=(value!=""?double.parse(value):0)-(costPriceController.text!=""?double.parse(costPriceController.text):0);
-                wholeSalersShare=temp*0.20;
-                chemistsShare=temp*0.20;
-                doctorsShare=temp*0.30;
-                myShare=temp-wholeSalersShare-chemistsShare-doctorsShare;
+                calculateValues();
               });
             },
           ),
@@ -94,32 +107,51 @@ class _MyHomePageState extends State<MyHomePage> {
             onChanged: (value)
             {
               setState(() {
-                double temp=(mrpPriceController.text!=""?double.parse(mrpPriceController.text):0)-(costPriceController.text!=""?double.parse(costPriceController.text):0);
-                wholeSalersShare=temp*(value!=""?double.parse(value)/100:0.2);
-                chemistsShare=temp*0.20;
-                doctorsShare=temp*0.30;
-                myShare=temp-wholeSalersShare-chemistsShare-doctorsShare;
+                calculateValues();
+              });
+            },
+          ),
+          TextFormField(
+            style: TextStyle(fontSize: 30),
+            controller: chemistSchemeController,
+            textInputAction: TextInputAction.next,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Chemist's Scheme (Percentage)",
+            ),
+            onChanged: (value)
+            {
+              setState(() {
+                calculateValues();
               });
             },
           ),
           SizedBox(height: 10,),
-          Text("Cost Price: ₹${(costPriceController.text!=""?double.parse(costPriceController.text):0).toStringAsFixed(2).replaceAllMapped(commaRegex, (match) => matchFunc(match))}",style: TextStyle(fontSize: 30),),
+          Text("Cost Price: ₹${cp.toStringAsFixed(2).replaceAllMapped(commaRegex, (match) => matchFunc(match))}",),
           SizedBox(height: 10,),
-          Text("MRP: ₹${(mrpPriceController.text!=""?double.parse(mrpPriceController.text):0).toStringAsFixed(2).replaceAllMapped(commaRegex, (match) => matchFunc(match))}",style: TextStyle(fontSize: 30),),
+          Text("MRP: ₹${mrp.toStringAsFixed(2).replaceAllMapped(commaRegex, (match) => matchFunc(match))}",),
           SizedBox(height: 30,),
-          Text("Wholesaler's Share: ₹${wholeSalersShare.toStringAsFixed(2).replaceAllMapped(commaRegex, (match) => matchFunc(match))}",style: TextStyle(fontSize: 30),),
+          Text("Wholesaler's Share: ₹${wholeSalersShare.toStringAsFixed(2).replaceAllMapped(commaRegex, (match) => matchFunc(match))}",),
           SizedBox(height: 10,),
-          Text("Wholesaler's Payment: ₹${(wholeSalersShare+(costPriceController.text!=""?double.parse(costPriceController.text):0)).toStringAsFixed(2).replaceAllMapped(commaRegex, (match) => matchFunc(match))}",style: TextStyle(fontSize: 30),),
+          Text("Wholesaler's Payment: ₹${(wholeSalersShare+cp).toStringAsFixed(2).replaceAllMapped(commaRegex, (match) => matchFunc(match))}",),
           SizedBox(height: 30,),
-          Text("My Share: ₹${myShare.toStringAsFixed(2).replaceAllMapped(commaRegex, (match) => matchFunc(match))}",style: TextStyle(fontSize: 30),),
+          Text("My Share: ₹${myShare.toStringAsFixed(2).replaceAllMapped(commaRegex, (match) => matchFunc(match))}",),
           SizedBox(height: 30,),
-          Text("Chemist's Share: ₹${chemistsShare.toStringAsFixed(2).replaceAllMapped(commaRegex, (match) => matchFunc(match))}",style: TextStyle(fontSize: 30),),
+          Text("Chemist's Share: ₹${chemistsShare.toStringAsFixed(2).replaceAllMapped(commaRegex, (match) => matchFunc(match))}",),
           SizedBox(height: 10,),
-          Text("Chemist's Price: ₹${((mrpPriceController.text!=""?double.parse(mrpPriceController.text):0)-chemistsShare).toStringAsFixed(2).replaceAllMapped(commaRegex, (match) => matchFunc(match))}",style: TextStyle(fontSize: 30),),
-          SizedBox(height: 30,),
-          Text("Doctor's share: ₹${doctorsShare.toStringAsFixed(2).replaceAllMapped(commaRegex, (match) => matchFunc(match))}",style: TextStyle(fontSize: 30),),
+          Text("Chemist's Profit: ₹${(chemistsShare+scheme*(mrp-chemistsShare)).toStringAsFixed(2).replaceAllMapped(commaRegex, (match) => matchFunc(match))}",),
           SizedBox(height: 10,),
-          Text("Doctor's Price: ₹${((mrpPriceController.text!=""?double.parse(mrpPriceController.text):0)-chemistsShare-doctorsShare).toStringAsFixed(2).replaceAllMapped(commaRegex, (match) => matchFunc(match))}",style: TextStyle(fontSize: 30),),
+          Text("Chemist's Price(exc GST): ₹${(chemistsPrice/1.18).toStringAsFixed(2).replaceAllMapped(commaRegex, (match) => matchFunc(match))}",),
+          SizedBox(height: 10,),
+          Text("Chemist's Price(icl GST): ₹${(chemistsPrice).toStringAsFixed(2).replaceAllMapped(commaRegex, (match) => matchFunc(match))}",),
+          SizedBox(height: 30,),
+          Text("Doctor's share(with chemist): ₹${doctorsShare.toStringAsFixed(2).replaceAllMapped(commaRegex, (match) => matchFunc(match))}",),
+          SizedBox(height: 10,),
+          Text("Doctor's share(without chemist): ₹${(doctorsShare+chemistsShare).toStringAsFixed(2).replaceAllMapped(commaRegex, (match) => matchFunc(match))}",),
+          SizedBox(height: 10,),
+          Text("Doctor's Price(exc GST): ₹${((doctorsPrice)/1.18).toStringAsFixed(2).replaceAllMapped(commaRegex, (match) => matchFunc(match))}",),
+          SizedBox(height: 10,),
+          Text("Doctor's Price(icl GST): ₹${(doctorsPrice).toStringAsFixed(2).replaceAllMapped(commaRegex, (match) => matchFunc(match))}",),
 
         ],
       ),
